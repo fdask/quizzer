1e528b0eec21934798937f339a899ecf23960092
James Wallace <sausages@gmail.com>
2014-05-27 17:30:00 -0400

    Added the categories page.  Lists category, number of questions in those categories, and lets you access the question listings or quiz pages via a link.

---
+++ cats.php
@@ -0,0 +1,29 @@
+<?php 
+require 'header.inc.php'; 
+?>
+<style>
+table, td {
+	border: 1px solid black;
+}
+</style>
+<table>
+	<tr>
+		<th>Category</th>
+		<th>Count</th>
+		<th>List</th>
+		<th>Quiz</th>
+	</tr>
+<?php
+$cats = getCategories();
+
+foreach ($cats as $cat) {
+	echo "<tr>";
+	echo "<td>{$cat['category']}</td>";
+	echo "<td>{$cat['count']}</td>";
+	echo "<td><a href='list.php?c={$cat['id']}'>List</a></td>";
+	echo "<td><a href='quiz.php?c={$cat['id']}'>Quiz</a></td>";
+	echo "</tr>";
+}
+
+echo "</table>";
+?>
